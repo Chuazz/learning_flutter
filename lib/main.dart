3f2426e98408d45d48173482342243516c16bc50
SonNV <sonnv1912@gmail.com>
2024-01-18 23:19:01 +0700

    day 2: styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      body: Center(child: Text('Hello world')),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromARGB(66, 0, 0, 0),
+          Color.fromARGB(255, 233, 153, 153),
+        ])),
+        child: const Center(
+            child: Text(
+          'Hello world',
+          style: TextStyle(fontSize: 20, color: Colors.amberAccent),
+        )),
+      ),
     ),
   ));
 }
