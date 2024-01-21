537c2d7df8c11269adbcc43b09fe7dfe35929fa9
SonNV <sonnv1912@gmail.com>
2024-01-21 10:15:20 +0700

    day 4: props

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:learning/components/custom.dart';
+import 'package:learning/components/my_typography.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: CustomComponent(),
+  runApp(MaterialApp(
+      home: Scaffold(
+    body: Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+        Color.fromARGB(66, 0, 0, 0),
+        Color.fromARGB(255, 233, 153, 153),
+      ])),
+      child: const Center(child: MTypography("Hello world")),
     ),
-  ));
+  )));
 }
